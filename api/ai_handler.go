fed491f4e0f1f74b5e69bda2f74e9df29cae263b
tf200 <farjiataha@gmail.com>
2025-02-03 09:57:14 +0100

    spelling check swagger

--- api/ai_handler.go
+++ api/ai_handler.go
@@ -6,15 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CorrectSpellingRequest is the request format for the spelling check API
 type CorrectSpellingRequest struct {
 	InitialText string `json:"initial_text"`
 }
 
+// CorrectSpellingResponse is the response format for the spelling check API
 type CorrectSpellingResponse struct {
 	CorrectedText string `json:"corrected_text"`
 	InitialText   string `json:"initial_text"`
 }
 
+// SpellingCheckApi is the handler for the spelling check API
+// @Summary Check spelling of a text
+// @Description Check spelling of a text
+// @Tags ai
+// @Accept json
+// @Produce json
+// @Param request body CorrectSpellingRequest true "Request body"
+// @Success 200 {object} Response[CorrectSpellingResponse]
+// @Router /ai/spelling_check [post]
 func (server *Server) SpellingCheckApi(ctx *gin.Context) {
 	var request CorrectSpellingRequest
 	if err := ctx.ShouldBindJSON(&request); err != nil {
