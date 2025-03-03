03fa4be5b2744c8a9b32d251a247d1493ac014e0
tf200 <farjiataha@gmail.com>
2025-03-03 22:42:08 +0000

    fix roles

--- api/roles_handler.go
+++ api/roles_handler.go
@@ -168,7 +168,7 @@ func (server *Server) ListRolesApi(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	response := make([]ListRolesApiResponse, 0)
+	response := make([]ListRolesApiResponse, len(roles))
 	for i, role := range roles {
 		response[i] = ListRolesApiResponse{
 			ID:   role.ID,
