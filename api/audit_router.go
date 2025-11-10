8e995be4acafc1cc0f425539e5a166f63d9e6673
tf200 <farjiataja@gmail.com>
2025-11-10 11:47:22 +0100

    fix audit routes

--- api/audit_router.go
+++ api/audit_router.go
@@ -2,7 +2,7 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-func (s *Server) SetupAuditRoutes(router *gin.RouterGroup) {
+func (s *Server) setupAuditRoutes(router *gin.RouterGroup) {
 	auditRoutes := router.Group("/audit")
 	{
 		auditRoutes.GET("/logs", s.RBACMiddleware("AUDIT_LOGS.VIEW"), s.ListAuditLogs)
