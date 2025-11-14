e837dcda7f6f7d03db75d3d42a298760a6cd6424
tf200 <farjiataja@gmail.com>
2025-11-14 09:25:04 +0100

    save

--- db/sqlc/employee_profile_test.go
+++ db/sqlc/employee_profile_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// old tests
 func createRandomEmployee(t *testing.T) (EmployeeProfile, *CustomUser) {
 	// Create prerequisite records
 	location := CreateRandomLocation(t)
