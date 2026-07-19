51b12c4d168f02d8b5ab89c7e5f8e1fc6290cf59
tf200 <farjiataha@gmail.com>
2026-07-19 17:25:42 +0100

    fix: allow registration token cors header

--- internal/app/app.go
+++ internal/app/app.go
@@ -431,7 +431,7 @@ func corsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Header("Access-Control-Allow-Origin", "*")
 		ctx.Header("Access-Control-Allow-Credentials", "true")
-		ctx.Header("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization")
+		ctx.Header("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization, X-Registration-Token")
 		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		if ctx.Request.Method == http.MethodOptions {
 			ctx.AbortWithStatus(http.StatusNoContent)
